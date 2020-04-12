643b4e471dfe556104360632b41c0c37f7e076e0
Divyansh <divyanshrajput337@gmail.com>
2020-04-12 11:28:16 +0530

    Another solution for Pollutant Mean

--- scripts/pollutantmean.R
+++ scripts/pollutantmean.R
@@ -1,30 +1,44 @@
 pollutantmean <- function(directory,pollutant,id = 1:332){
     
-    data <- data.frame()  
+    # data <- data.frame()
+    # for(i in id)
+    # {
+    #     #print(i)
+    #     if(i<10)
+    #     {
+    #         path <- paste(getwd(),"/",directory,"/00",as.character(i),".csv",sep = "")
+    #         dat <- read.csv(path)
+    #         data <- rbind(data,dat)
+    #     }
+    #     else if(i<100)
+    #     {
+    #         path <- paste(getwd(),"/",directory,"/0",as.character(i),".csv",sep = "")
+    #         dat <- read.csv(path)
+    #         data <- rbind(data,dat)
+    #     }
+    #     else
+    #     {
+    #         path <- paste(getwd(),"/",directory,"/",as.character(i),".csv",sep = "")
+    #         dat <- read.csv(path)
+    #         data <- rbind(data,dat)
+    #     }
+    #     #print(path)
+    #     #print(ncol(data))
+    # }
+    # #print(data[,pollutant])
+    # mean(data[,pollutant],na.rm = TRUE)
+    
+    #ANOTHER SOLUTION
+    
+    data <- data.frame()
+    
+    csvFiles <- list.files(directory,full.names = TRUE)
+    
     for(i in id)
     {
-        #print(i)
-        if(i<10)
-        {
-            path <- paste(getwd(),"/",directory,"/00",as.character(i),".csv",sep = "")
-            dat <- read.csv(path)
-            data <- rbind(data,dat)
-        }
-        else if(i<100)
-        {
-            path <- paste(getwd(),"/",directory,"/0",as.character(i),".csv",sep = "")
-            dat <- read.csv(path)
-            data <- rbind(data,dat)
-        }
-        else
-        {
-            path <- paste(getwd(),"/",directory,"/",as.character(i),".csv",sep = "")
-            dat <- read.csv(path)
-            data <- rbind(data,dat)
-        }
-        #print(path)
-        #print(ncol(data))
+        x <- read.csv(csvFiles[i])
+        data <- rbind(data,x)
     }
-    #print(data[,pollutant])
+    
     mean(data[,pollutant],na.rm = TRUE)
 }
