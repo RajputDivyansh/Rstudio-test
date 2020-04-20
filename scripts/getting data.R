fc24d1f71c56edc05e5dba7baca20cc199c217d2
Divyansh <divyanshrajput337@gmail.com>
2020-04-20 15:10:47 +0530

    json and data.read package

--- scripts/getting data.R
+++ scripts/getting data.R
@@ -69,3 +69,77 @@ xpathSApply(rootNode,"//price",xmlValue)
 
 ##OUTPUT
 #[1] "$5.95" "$7.95" "$8.95" "$4.50" "$6.95"
+
+
+
+
+##JSON reading
+library(jsonlite)
+
+jsonData <- fromJSON("https://api.github.com/users/jtleek/repos")
+names(jsonData)
+
+
+#converting data to JSON
+myjson <- toJSON(iris,pretty = TRUE)
+myjson
+
+
+#back to orginal form from JSON
+iris2 <- fromJSON(myjson)
+head(iris2)
+
+
+
+
+##DATA.TABLE package
+
+library(data.table)
+DT=data.table(x = rnorm(9),y=rep(c("a","b","c"),each=3),z=rnorm(9))
+head(DT,4)
+
+#subsetting rows in table
+DT[2,]
+
+#subsetting where y=a
+DT[DT$y=="a",]
+
+#Using function in index
+DT[,list(mean(x),sum(z))]
+
+#Adding new columns
+DT[,w:=z^2]       ##it will add column to the table without replicating the orignal table as data.frame doesn't
+
+#Assigning data to new variable
+DT2 <- DT       ##any changes occur in DT will reflect in DT2
+                ##to stop this use copy function to copy data to another variable
+
+#MULTIPLE OPERATION
+DT[,m:={tmp <- (x+z); log2(tmp+5)}]
+
+#plyr like operation
+DT[,a:=x>0]
+
+##by=a means grouped by a
+DT[,b:=mean(x+w),by=a]
+
+DT1 <- data.table(x=sample(letters[1:3],1E5,TRUE))
+##.N counts the number of times grouped by varaible by x
+DT1[,.N,by=x]
+
+
+##KEYS
+
+DT2 <- data.table(x = rep(c("a","b","c"),each = 100),y = rnorm(300))
+setkey(DT2,x)
+DT2['a']
+
+
+##JOINS USING KEYS
+
+DT1 <- data.table(x = c('a','a','b','dt1'),y = 1:4)
+DT2 <- data.table(x = c('a','b','dt2'),z = 5:7)
+setkey(DT1,x)
+setkey(DT2,x)
+merge(DT1,DT2)
+
