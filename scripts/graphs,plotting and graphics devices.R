b49e09b750968b8a6fb2ce9d5551b4767ed6801d
RajputDivyansh <divyanshrajput337@gmail.com>
2020-06-22 09:17:00 +0530

    other graphing techniques

--- scripts/graphs,plotting and graphics devices.R
+++ scripts/graphs,plotting and graphics devices.R
@@ -91,3 +91,44 @@ p <- g + geom_point(aes(color = drv)) + theme_bw(base_family = "Times")
 
 #Axis limit in case of outlier
 g + geom_line() + coord_cartesian(ylim = c(-3,3))
+
+
+
+##HIERARCHICAL CLUSTERING
+set.seed(1234)
+par(mar = c(0,0,0,0))
+x <- rnorm(12,mean = rep(1:3,each = 4),sd = 0.2)
+y <- rnorm(12,mean = rep(c(1,2,1),each = 4),sd = 0.2)
+plot(x,y,col = "blue", pch =19,cex = 2)
+text(x + 0.05, y + 0.05,labels = as.character(1:12))
+
+#Clustering - hclust
+dataFrame <- data.frame(x=x,y=y)
+distxy <- dist(dataFrame)
+
+hClustering <- hclust(distxy)
+plot(hClustering)
+
+
+
+##KMEANS()
+dataFrame <- data.frame(x=x,y=y)
+kmeansObj <- kmeans(dataFrame,centers = 3)
+names(kmeansObj)
+
+par( mar = rep(2,4))
+plot(x,y,col = kmeansObj$cluster,pch = 19,cex = 2)
+points(kmeansObj$centers,col = 1:3,pch = 3,lwd = 3)
+
+
+
+##RCOLORBREWER and COLORRAMPPALETTE
+library(RColorBrewer)
+cols <- brewer.pal(3,"BuGn")
+pal <- colorRampPalette(cols)
+image(volcano,col = pal(20))
+
+#smoothScatter
+x <- rnorm(10000)
+y <- rnorm(10000)
+smoothScatter(x,y)
